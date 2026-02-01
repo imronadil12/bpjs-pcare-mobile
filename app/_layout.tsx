5fc2892589daeb937cd24b41917e17d88d403bb2
imronadil <imronadil@yahoo.com>
2026-02-02 04:06:25 +0700

    starting point

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,10 +6,6 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { AutomationProvider } from '@/context/AutomationContext';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
@@ -17,7 +13,7 @@ export default function RootLayout() {
     <AutomationProvider>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="pcare" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
         </Stack>
         <StatusBar style="auto" />
